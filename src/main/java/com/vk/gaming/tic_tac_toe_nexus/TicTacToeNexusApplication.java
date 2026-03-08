e2b61989737db1a102cfc3cb8a6e3b4d65a70a6c
Vijay Kumar <94276052+VijayKumarCode@users.noreply.github.com>
2026-03-08 21:58:52 +0530

    chore: initialize spring boot nexus gaming core with java 17

---
+++ src/main/java/com/vk/gaming/tic_tac_toe_nexus/TicTacToeNexusApplication.java
@@ -0,0 +1,13 @@
+package com.vk.gaming.tic_tac_toe_nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/gaming/tic_tac_toe_nexus/TicTacToeNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.gaming.tic_tac_toe_nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
